59282ef7773cf2d1ea9bb768ecc3acc5a77de7ea
Snow Leopard <myfly245@gmail.com>
2022-02-12 15:07:12 +0300

    5-7

--- main.py
+++ main.py
@@ -18,23 +18,37 @@ class request():
     def __init__(self):
         self.api_server = "http://static-maps.yandex.ru/1.x/"
 
-    def get(self, lon, lat, delta, type_map):
-
+    def get(self, lon, lat, delta, type_map, lon_pt, lat_pt):
         params = {
             "ll": ",".join([lon, lat]),
             "spn": ",".join([delta, delta]),
-            "l": type_map}
+            "l": type_map,
+            "pt": ",".join([lon_pt, lat_pt, "ya_ru"])}
         response = requests.get(self.api_server, params=params)
         if not response:
             response = "bad"
         return response
-
+    
+    def search(self, location):
+        geocoder_request = "http://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": location,
+            "format": "json"}
+        response = requests.get(geocoder_request, params)
+        if response:
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            toponym_coodrinates = toponym_coodrinates.split()
+        else:
+            toponym_coodrinates = None
+        return toponym_coodrinates
 
 def to_img(response):
-    img = Image.new("RGB", (600, 450), (256, 256, 256))
-    code = Image.open(io.BytesIO(response))
-    img.paste(code, (0, 0))
-    img.save("Map.png")
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
 
 class OptionBox():
@@ -141,13 +155,17 @@ def main():
     input_box1 = UITextEntryLine(relative_rect=Rect(100, 100, 100, 32), manager=manager)
     input_box2 = UITextEntryLine(relative_rect=Rect(100, 200, 100, 32), manager=manager)
     input_box3 = UITextEntryLine(relative_rect=Rect(100, 300, 100, 32), manager=manager)
-    dropbox_type = OptionBox(800, 100, 100, 32, (240, 240, 240), (40, 40, 40),
+    input_box4 = UITextEntryLine(relative_rect=Rect(900, 100, 100, 32), manager=manager)
+    dropbox_type = OptionBox(900, 150, 100, 32, (240, 240, 240), (40, 40, 40),
                                      pg.font.SysFont("Segoe UI", 30), ["map", "satellite", "hybrid"])
-    lon = ""
-    lat = ""
-    delta = ""
-    type_map = "map"
     button = Button((100, 400), butWidth=140, text="map")
+    button2 = Button((900, 400), butWidth=140, text="search")                                  
+    lon = "30"
+    lat = "60"
+    lon_pt, lat_pt = "0", "0"
+    delta = "1"
+    type_map = "map"
+    location = ""
     done = False
     img = "None.png"
     font = pg.font.SysFont("Segoe UI", 30)
@@ -155,55 +173,65 @@ def main():
         events = pg.event.get()
         mousePos = pg.mouse.get_pos()
         for event in events:
+            try:
+                map_zoom = {float(delta) >= 4: 1, 4 > float(delta) > 1: 0.5,
+                 1 >= float(delta) >= 0.2: 0.1, 0.2 > float(delta) >= 0.02: 0.01,
+                  0.02 > float(delta) >= 0.002: 0.001, 0.002 > float(delta) >= 0.0002: 0.0001}[True]
+            except:
+                map_zoom = 0.0001
             if event.type == pg.QUIT:
                 done = True
             if event.type == pg.USEREVENT:
                 if event.user_type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
                     if event.ui_element == input_box1:
                         lon = event.text
-                    if event.ui_element == input_box2:
+                    elif event.ui_element == input_box2:
                         lat = event.text
-                    if event.ui_element == input_box3:
+                    elif event.ui_element == input_box3:
                         delta = event.text
+                    elif event.ui_element == input_box4:
+                        location = event.text
             if event.type == pg.KEYUP and event.key == pg.K_PAGEDOWN:
-                delta_fl = float(delta) + 0.01
+                delta_fl = float(delta) + map_zoom
                 if delta_fl <= 90:
                     delta = str(delta_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.KEYUP and event.key == pg.K_PAGEUP:
-                delta_fl = float(delta) - 0.01
+                delta_fl = float(delta) - map_zoom
                 if delta_fl >= 0:
                     delta = str(delta_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.KEYUP and event.key == pg.K_RIGHT:
-                lon_fl = float(lon) + 0.01
+                lon_fl = float(lon) + map_zoom
                 if lon_fl < 180:
                     lon = str(lon_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.KEYUP and event.key == pg.K_LEFT:
-                lon_fl = float(lon) - 0.01
+                lon_fl = float(lon) - map_zoom
                 if lon_fl > -180:
                     lon = str(lon_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.KEYUP and event.key == pg.K_UP:
-                lat_fl = float(lat) + 0.01
+                lat_fl = float(lat) + map_zoom
                 if lat_fl < 180:
                     lat = str(lat_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.KEYUP and event.key == pg.K_DOWN:
-                lat_fl = float(lat) - 0.01
+                lat_fl = float(lat) - map_zoom
                 if lat_fl > -180:
                     lat = str(lat_fl)
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
             if event.type == pg.MOUSEBUTTONDOWN and button.mouse_in(mousePos) is True:
-                img = map(lon, lat, delta, type_map)
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt, "map")
+            elif event.type == pg.MOUSEBUTTONDOWN and button2.mouse_in(mousePos) is True:
+                img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt, "location", location)
             manager.process_events(event)
             drop_type = dropbox_type.update(events)
             if drop_type != -1:
                 old = type_map
                 type_map = ["map", "sat", "sat,skl"][drop_type]
                 if old != type_map:
-                    img = map(lon, lat, delta, type_map)
+                    img, lon, lat, delta, lon_pt, lat_pt = map(lon, lat, delta, type_map, lon_pt, lat_pt)
         screen.fill((30, 30, 30))
         manager.update(clock.tick(30)/1000.0)
         manager.draw_ui(screen)
@@ -214,28 +242,37 @@ def main():
         try:
             lon_txt = font.render(f"lon: {lon[:5]}", True, (112, 112, 112))
             lat_txt = font.render(f"lat: {lat[:5]}", True, (112, 112, 112))
-            delta_txt = font.render(f"delta: {delta[:5]}", True, (112, 112, 112))
+            delta_txt = font.render(f"delta: {delta[:7]}", True, (112, 112, 112))
             type_map_txt = font.render(f"type: {type_map[:5]}", True, (112, 112, 112))
+            location_txt = font.render(f"location: {location}", True, (112, 112, 112))
             screen.blit(lon_txt, (100, 500))
             screen.blit(lat_txt, (100, 550))
             screen.blit(delta_txt, (100, 600))
             screen.blit(type_map_txt, (100, 650))
+            screen.blit(location_txt, (100, 700))
         except:
             pass
         button.render(screen)
+        button2.render(screen)
         pg.display.flip()
         clock.tick(30)
 
-def map(lon, lat, delta, type_map):
+def map(lon, lat, delta, type_map, lon_pt, lat_pt, type_call: str = "none", location: str = "none"):
     api_map = request()
-    bytes_code = api_map.get(lon, lat, delta, type_map)
+    if type_call == "location":
+        print(api_map.search(location))
+        lon, lat = api_map.search(location)
+        delta = "0.002"
+        lon_pt, lat_pt = lon, lat
+    if type_call == "map":
+        lon_pt, lat_pt = "0", "0"
+    bytes_code = api_map.get(lon, lat, delta, type_map, lon_pt, lat_pt)
     if bytes_code != "bad":
-        bytes_code = bytes_code.content
         to_img(bytes_code)
         img = "Map.png"
     else:
-        img ="None.png"
-    return img
+        img = "None.png"
+    return img, lon, lat, delta, lon_pt, lat_pt
 
 if __name__ == '__main__':
     main()
